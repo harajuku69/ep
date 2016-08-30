9b73f4aa07a2a90bdb7121beb1c612efeeb7abe3
정일대 <harajuku69korea@gmail.com>
2016-08-31 03:06:08 +0900

    0831-0306 get rid of item

---
+++ ep/src/com/mars/common/action/AdminUpdateAction.java
@@ -0,0 +1,43 @@
+package com.mars.common.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mars.staff.dao.StaffDao;
+import com.mars.staff.dto.StaffDto;
+
+public class AdminUpdateAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "staff/adminDetail.jsp";
+		
+		String empid = SS.getEmpid(request);
+		
+		StaffDto sDto = new StaffDto();
+		sDto.setDptcd(request.getParameter("dptcd"));
+		sDto.setTitcd(request.getParameter("titcd"));
+		sDto.setStartdt(Fmt.strToTimestamp(request.getParameter("startdt")));
+		sDto.setEnddt(Fmt.strToTimestamp(request.getParameter("enddt")));
+		sDto.setSal(Integer.parseInt(request.getParameter("sal")));
+		sDto.setAdmchk(Integer.parseInt(request.getParameter("admchk")));
+		sDto.setEmpid(empid);
+//		System.out.println("업데이트 전 : " +sDto);
+		StaffDao sDao = StaffDao.getInstance();
+		sDao.updateStaffEmpInfo(sDto);
+		//DB에 덥데이트 완료. 이후는 detail 로 가기 위해 다시 정보 가져와서 마는 작업
+		
+		sDto = sDao.selectOneByEmpid(empid);
+//		System.out.println("업데이트 후 : " +sDto);
+		Fmt.toFmtAndReqSet(request, sDto);
+		
+		RequestDispatcher disp = request.getRequestDispatcher(url);
+		disp.forward(request, response);
+
+	}
+
+}
